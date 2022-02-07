243059b9256df629b81604b344be06242235c264
BradyBassett <bradydbassett@gmail.com>
2022-02-06 20:07:54 -0600

    init

--- src/com/C195/Main.java
+++ src/com/C195/Main.java
@@ -1,11 +1,33 @@
 package com.C195;
 
 import helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/c482performanceassessment/main-form.fxml")));
+            Scene mainScene = new Scene(root);
+            stage.setTitle("Inventory Management System");
+            stage.setResizable(false);
+            stage.setScene(mainScene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         JDBC.openConnection();
+        launch();
         JDBC.closeConnection();
     }
 }
