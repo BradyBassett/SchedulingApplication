03e37c1a4d4750809931d654398d271055529ba0
BradyBassett <bradydbassett@gmail.com>
2022-02-09 15:33:38 -0600

    added showAlert function

--- src/com/C195/Controllers/BaseFormController.java
+++ src/com/C195/Controllers/BaseFormController.java
@@ -1,13 +1,18 @@
 package com.C195.Controllers;
 
+import javafx.scene.control.Alert;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import static helper.JDBC.*;
+
 public abstract class BaseFormController {
     protected static Locale locale = Locale.getDefault();
-    protected static ResourceBundle bundle = setBundle();
+    protected static ResourceBundle bundle = ResourceBundle.getBundle("com.C195.language", locale);
 
-    private static ResourceBundle setBundle() {
-        return ResourceBundle.getBundle("com.C195.language", locale);
+    protected void showAlert(Exception e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, e.toString());
+        alert.showAndWait();
     }
 }
