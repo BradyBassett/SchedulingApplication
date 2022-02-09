b061bda0172806ee2655a724a6a8f56668c2048f
BradyBassett <bradydbassett@gmail.com>
2022-02-09 15:36:03 -0600

    began onLogin function

--- src/com/C195/Controllers/LoginFormController.java
+++ src/com/C195/Controllers/LoginFormController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -14,13 +13,14 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static helper.JDBC.executeQuery;
+
+
 public class LoginFormController extends BaseFormController implements Initializable {
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
     @FXML private Button loginBtn;
     @FXML private Label zoneLabel;
-    private Stage stage;
-    private Parent root;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -29,9 +29,21 @@ public class LoginFormController extends BaseFormController implements Initializ
         zoneLabel.setText(String.valueOf(locale));
     }
 
-    @FXML private void onLogin() {
-        System.out.println(usernameField.getText());
-        System.out.println(passwordField.getText());
-        System.out.println(loginBtn);
+    @FXML private void onLogin(ActionEvent event) {
+        try {
+            loginValidation();
+            executeQuery();
+        } catch (NullPointerException e) {
+            showAlert(e);
+            return;
+        }
+        ViewController viewController = new ViewController();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        viewController.showView(stage, "../Views/Login/loginView.fxml");
+    }
+
+    private void loginValidation() {
+        if (usernameField.getText().isEmpty()) throw new NullPointerException(bundle.getString("error.NullUsername"));
+        if (passwordField.getText().isEmpty()) throw new NullPointerException(bundle.getString("error.Nullpassword"));
     }
 }
