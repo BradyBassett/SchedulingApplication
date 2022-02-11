1ff64b67d808025d93ab1d1ee5e2869b66f8d94f
BradyBassett <bradydbassett@gmail.com>
2022-02-11 14:18:12 -0600

    made locale and bundle variables final

--- src/com/C195/Controllers/BaseFormController.java
+++ src/com/C195/Controllers/BaseFormController.java
@@ -6,8 +6,8 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public abstract class BaseFormController {
-    protected static Locale locale = Locale.getDefault();
-    protected static ResourceBundle bundle = ResourceBundle.getBundle("com.C195.language", locale);
+    protected final static Locale locale = Locale.getDefault();
+    protected final static ResourceBundle bundle = ResourceBundle.getBundle("com.C195.Resources.language", locale);
 
     protected void showAlert(Exception e) {
         Alert alert = new Alert(Alert.AlertType.ERROR, e.toString());
