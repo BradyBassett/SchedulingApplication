e1f492e921f2ce4d7a7b773050b6da30bd6a629b
BradyBassett <bradydbassett@gmail.com>
2022-02-16 22:43:30 -0600

    table now is initialized with appointments query

--- src/com/C195/Controllers/MainViewController.java
+++ src/com/C195/Controllers/MainViewController.java
@@ -1,17 +1,27 @@
 package com.C195.Controllers;
 
 import com.C195.Models.Appointment;
-import com.C195.Models.Contact;
-import com.C195.Models.Customer;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import static com.C195.Database.JDBC.closeConnection;
+import static com.C195.Database.JDBC.openConnection;
+import static com.C195.Database.QueryAppointments.getAppointments;
+import static com.C195.Database.QueryAppointments.queryAppointments;
+
 public class MainViewController extends ViewController implements Initializable {
+    private ObservableList<Appointment> appointments = FXCollections.observableArrayList();
     @FXML private Tab appointmentsTab;
     @FXML private Tab customersTab;
     @FXML private Button addButton;
@@ -25,45 +35,24 @@ public class MainViewController extends ViewController implements Initializable 
     @FXML private TableColumn<Appointment, String> appointmentType;
     @FXML private TableColumn<Appointment, String> appointmentStart;
     @FXML private TableColumn<Appointment, String> appointmentEnd;
-    @FXML private TableColumn<Appointment, Customer> appointmentCustomer;
-    @FXML private TableColumn<Appointment, Contact> appointmentContact;
+    @FXML private TableColumn<Appointment, String> appointmentCustomer;
+    @FXML private TableColumn<Appointment, String> appointmentContact;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setText();
+        initText();
+        initAppointmentTable();
     }
 
-    @FXML private void handleTabSelection() {
-
-    }
-
-    @FXML private void handleAdd() {
-
-    }
-
-    @FXML private void handleModify() {
-
-    }
-
-    @FXML private void handleDelete() {
-
-    }
-
-    @FXML private void handleDateSelect() {
-
-    }
-
-    private void setTableValues() {
-
-    }
-
-    private void setText() {
+    private void initText() {
         appointmentsTab.setText(bundle.getString("tab.appointments"));
         customersTab.setText(bundle.getString("tab.customers"));
         addButton.setText(bundle.getString("button.add"));
         modifyButton.setText(bundle.getString("button.modify"));
         deleteButton.setText(bundle.getString("button.delete"));
         selectedDate.setValue(LocalDate.now());
+
+        appointmentTable.setPlaceholder(new Label(bundle.getString("table.empty")));
         appointmentTitle.setText(bundle.getString("table.title"));
         appointmentDescription.setText(bundle.getString("table.description"));
         appointmentLocation.setText(bundle.getString("table.location"));
@@ -72,5 +61,49 @@ public class MainViewController extends ViewController implements Initializable 
         appointmentEnd.setText(bundle.getString("table.end"));
         appointmentCustomer.setText(bundle.getString("table.customer"));
         appointmentContact.setText(bundle.getString("table.contact"));
+    }
+
+    private void initAppointmentTable() {
+        getAppointmentsArray();
+
+        appointmentTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        appointmentDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        appointmentLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
+        appointmentType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        appointmentStart.setCellValueFactory(new PropertyValueFactory<>("start"));
+        appointmentEnd.setCellValueFactory(new PropertyValueFactory<>("end"));
+        // cd means cell data
+        appointmentCustomer.setCellValueFactory(cd -> new SimpleStringProperty(cd.getValue().getCustomer().getCustomerName()));
+        appointmentContact.setCellValueFactory(cd -> new SimpleStringProperty(cd.getValue().getContact().getContactName()));
+        appointmentTable.setItems(appointments);
+    }
+
+    private void getAppointmentsArray() {
+        try {
+            openConnection();
+            queryAppointments(Date.valueOf(selectedDate.getValue()));
+        } catch (SQLException e) {
+            showAlert(e);
+        } finally {
+            closeConnection();
+        }
+
+        appointments = FXCollections.observableArrayList(getAppointments());
+    }
+
+    @FXML private void handleAppointmentDateSelect() {
+        initAppointmentTable();
+    }
+
+    @FXML private void handleAppointmentAdd() {
+
+    }
+
+    @FXML private void handleAppointmentModify() {
+
+    }
+
+    @FXML private void handleAppointmentDelete() {
+
     }
 }
