1d31f431f9c231035bfc9890abefef562569c1b7
BradyBassett <bradydbassett@gmail.com>
2022-02-18 18:26:56 -0600

    overloaded showView function for ActionEvent parameters

--- src/com/C195/Controllers/ViewController.java
+++ src/com/C195/Controllers/ViewController.java
@@ -1,10 +1,13 @@
 package com.C195.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class ViewController extends BaseController {
@@ -13,11 +16,18 @@ public class ViewController extends BaseController {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(view)));
             Scene scene = new Scene(root);
             stage.setTitle(bundle.getString("app.title"));
-            stage.setResizable(false);
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    protected void showView(ActionEvent e, String view) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(view)));
+        Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
