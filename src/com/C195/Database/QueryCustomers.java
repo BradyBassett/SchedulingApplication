efd58a333ade4c2bcee66810c81f828280c6a742
BradyBassett <bradydbassett@gmail.com>
2022-02-18 19:14:59 -0600

    Added queryCustomers

--- src/com/C195/Database/QueryCustomers.java
+++ src/com/C195/Database/QueryCustomers.java
@@ -1,11 +1,11 @@
 package com.C195.Database;
 
 import com.C195.Models.Customer;
-import com.C195.Models.Division;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static com.C195.Database.JDBC.connection;
 import static com.C195.Database.QueryDivisions.getCurrentDivision;
@@ -13,6 +13,7 @@ import static com.C195.Database.QueryDivisions.queryDivision;
 
 public abstract class QueryCustomers extends Query {
     private static Customer currentCustomer;
+    private static ArrayList<Customer> customers = new ArrayList<>();
 
     public static Customer getCurrentCustomer() {
         return currentCustomer;
@@ -22,11 +23,14 @@ public abstract class QueryCustomers extends Query {
         currentCustomer = customer;
     }
 
+    public static ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
     public static void queryCustomer(int customerId) throws SQLException {
         String statement = "SELECT * " +
                 "FROM customers " +
                 "WHERE Customer_ID=" + customerId + ";";
-
         setPreparedStatement(connection, statement);
         PreparedStatement preparedStatement = getPreparedStatement();
         preparedStatement.execute();
@@ -37,6 +41,22 @@ public abstract class QueryCustomers extends Query {
                     results.getString("Address"), results.getString("Postal_Code"),
                     results.getString("Phone"), getCurrentDivision());
             setCurrentCustomer(customer);
+        }
+    }
+
+    public static void queryCustomers() throws SQLException {
+        customers = new ArrayList<>();
+        String statement = "SELECT * FROM customers";
+        setPreparedStatement(connection, statement);
+        PreparedStatement preparedStatement = getPreparedStatement();
+        preparedStatement.execute();
+        ResultSet results = preparedStatement.getResultSet();
+        while (results.next()) {
+            queryDivision(results.getInt("Division_ID"));
+            Customer customer = new Customer(results.getInt("Customer_ID"), results.getString("Customer_Name"),
+                    results.getString("Address"), results.getString("Postal_Code"),
+                    results.getString("Phone"), getCurrentDivision());
+            customers.add(customer);
         }
     }
 }
