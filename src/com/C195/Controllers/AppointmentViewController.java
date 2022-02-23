394974be1bbadc097952b3dd0089e03c1b7038e6
BradyBassett <bradydbassett@gmail.com>
2022-02-22 18:53:49 -0600

    init

--- src/com/C195/Controllers/AppointmentViewController.java
+++ src/com/C195/Controllers/AppointmentViewController.java
@@ -1,4 +1,21 @@
 package com.C195.Controllers;
 
-public class AppointmentViewController extends FormController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class AppointmentViewController extends ViewController {
+    @FXML
+    private Button cancelButton;
+    @FXML private Button saveButton;
+
+    @FXML private void handleCancel(ActionEvent e) throws IOException {
+        showView(e, "../Views/mainView.fxml");
+    }
+
+    @FXML private void handleSave(ActionEvent e) throws IOException {
+        showView(e, "../Views/mainView.fxml");
+    }
 }
