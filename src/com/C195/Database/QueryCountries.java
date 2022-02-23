c2eea7658cd7cd5ade594134ee4d7bc7ed79cacb
BradyBassett <bradydbassett@gmail.com>
2022-02-22 18:49:03 -0600

    added function to query all countries

--- src/com/C195/Database/QueryCountries.java
+++ src/com/C195/Database/QueryCountries.java
@@ -2,17 +2,22 @@ package com.C195.Database;
 
 import com.C195.Models.Country;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static com.C195.Database.JDBC.connection;
 
 public abstract class QueryCountries extends Query {
     private static Country currentCountry;
+    private static final ArrayList<Country> countries = new ArrayList<>();
 
     public static Country getCurrentCountry() {
         return currentCountry;
+    }
+
+    public static ArrayList<Country> getCountries() {
+        return countries;
     }
 
     private static void setCurrentCountry(Country country) {
@@ -23,14 +28,19 @@ public abstract class QueryCountries extends Query {
         String statement = "SELECT * " +
                 "FROM countries " +
                 "WHERE Country_ID=" + countryId + ";";
-
-        setPreparedStatement(connection, statement);
-        PreparedStatement preparedStatement = getPreparedStatement();
-        preparedStatement.execute();
-        ResultSet results = preparedStatement.getResultSet();
+        ResultSet results = getResults(connection, statement);
         if (results.next()) {
             Country country = new Country(countryId, results.getString("Country"));
             setCurrentCountry(country);
+        }
+    }
+
+    public static void queryCountries() throws SQLException {
+        String statement = "SELECT * FROM countries;";
+        ResultSet results = getResults(connection, statement);
+        while (results.next()) {
+            Country country = new Country(results.getInt("Country_ID"), results.getString("Country"));
+            countries.add(country);
         }
     }
 }
