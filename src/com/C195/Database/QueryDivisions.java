3f8bb2992573a997d38c01f68f0663036f2b2916
BradyBassett <bradydbassett@gmail.com>
2022-02-22 18:48:23 -0600

    added function to query list of all divisions with given country id

--- src/com/C195/Database/QueryDivisions.java
+++ src/com/C195/Database/QueryDivisions.java
@@ -2,9 +2,9 @@ package com.C195.Database;
 
 import com.C195.Models.Division;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static com.C195.Database.JDBC.connection;
 import static com.C195.Database.QueryCountries.getCurrentCountry;
@@ -12,9 +12,14 @@ import static com.C195.Database.QueryCountries.queryCountry;
 
 public abstract class QueryDivisions extends Query {
     private static Division currentDivision;
+    private static ArrayList<Division> divisions = new ArrayList<>();
 
     public static Division getCurrentDivision() {
         return currentDivision;
+    }
+
+    public static ArrayList<Division> getDivisions() {
+        return divisions;
     }
 
     private static void setCurrentDivision(Division division) {
@@ -25,15 +30,25 @@ public abstract class QueryDivisions extends Query {
         String statement = "SELECT * " +
                 "FROM first_level_divisions " +
                 "WHERE Division_ID=" + divisionId + ";";
-
-        setPreparedStatement(connection, statement);
-        PreparedStatement preparedStatement = getPreparedStatement();
-        preparedStatement.execute();
-        ResultSet results = preparedStatement.getResultSet();
+        ResultSet results = getResults(connection, statement);
         if (results.next()) {
             queryCountry(results.getInt("Country_ID"));
             Division division = new Division(divisionId, results.getString("Division"), getCurrentCountry());
             setCurrentDivision(division);
+        }
+    }
+
+    public static void queryDivisions(int countryId) throws SQLException {
+        divisions = new ArrayList<>();
+        String statement = "SELECT * " +
+                           "FROM first_level_divisions " +
+                           "WHERE Country_ID=" + countryId + ";";
+        ResultSet results = getResults(connection, statement);
+        while (results.next()) {
+            QueryCountries.queryCountry(countryId);
+            Division division = new Division(results.getInt("Division_ID"), results.getString("Division"),
+                    getCurrentCountry());
+            divisions.add(division);
         }
     }
 }
