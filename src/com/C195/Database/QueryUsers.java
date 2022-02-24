7d709cef7d127e9583d9590ff3ff25dd03211812
BradyBassett <bradydbassett@gmail.com>
2022-02-24 15:06:42 -0600

    queryUser function now returns a user

--- src/com/C195/Database/QueryUsers.java
+++ src/com/C195/Database/QueryUsers.java
@@ -9,16 +9,23 @@ import static com.C195.Controllers.BaseController.setCurrentUser;
 import static com.C195.Database.JDBC.*;
 
 public abstract class QueryUsers extends Query {
-    public static void queryUser(String username, String password) throws SQLException {
-        String statement = "SELECT * " +
-                           "FROM users " +
-                           "WHERE User_Name='" + username + "' " +
-                           "AND Password='" + password + "';";
+    public static void setCurrUser(String username, String password) throws SQLException {
+        String statement = "SELECT * FROM users WHERE User_Name='" + username + "' AND Password='" + password + "';";
         ResultSet results = getResults(connection, statement);
         if (results.next()) {
             User user = new User(results.getInt("User_ID"), results.getString("User_Name"),
                     results.getString("Password"));
             setCurrentUser(user);
         }
+    }
+
+    public static User queryUser(int userId) throws SQLException {
+        String statement = "SELECT * FROM users WHERE User_ID=" + userId + ";";
+        ResultSet results = getResults(connection, statement);
+        if (results.next()) {
+            return new User(userId, results.getString("User_Name"),
+                    results.getString("Password"));
+        }
+        return null;
     }
 }
