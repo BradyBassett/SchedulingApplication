84193d4ca7039266eea5f4989412ec9e12f6681b
BradyBassett <bradydbassett@gmail.com>
2022-02-28 18:32:34 -0600

    timezone conversion methods

--- src/com/C195/Controllers/BaseController.java
+++ src/com/C195/Controllers/BaseController.java
@@ -4,9 +4,11 @@ import com.C195.Models.User;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
-import java.util.Locale;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
 
 public abstract class BaseController {
     protected final static Locale locale = Locale.getDefault();
@@ -35,5 +37,35 @@ public abstract class BaseController {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, message);
         Optional<ButtonType> confirmation = alert.showAndWait();
         return confirmation.isPresent() && confirmation.get() == ButtonType.OK;
+    }
+
+    protected Timestamp convertLocalToUTC(Timestamp timestamp) {
+        ZoneId localZone = TimeZone.getDefault().toZoneId();
+        ZoneId utcZone = ZoneId.of("UTC");
+        LocalDateTime ldt = timestamp.toLocalDateTime();
+        ZonedDateTime localTime = ZonedDateTime.of(ldt, localZone);
+        ZonedDateTime utc = localTime.withZoneSameInstant(utcZone);
+
+        return Timestamp.valueOf(utc.toLocalDateTime());
+    }
+
+    protected Timestamp convertLocalToEST(Timestamp timestamp) {
+        ZoneId localZone = TimeZone.getDefault().toZoneId();
+        ZoneId estZone = ZoneId.of("America/New_York");
+        LocalDateTime ldt = timestamp.toLocalDateTime();
+        ZonedDateTime local = ZonedDateTime.of(ldt, localZone);
+        ZonedDateTime est = local.withZoneSameInstant(estZone);
+
+        return Timestamp.valueOf(est.toLocalDateTime());
+    }
+
+    protected Timestamp convertUTCToLocal(Timestamp timestamp) {
+        ZoneId utcZone = ZoneId.of("UTC");
+        ZoneId localZone = TimeZone.getDefault().toZoneId();
+        LocalDateTime ldt = timestamp.toLocalDateTime();
+        ZonedDateTime utc = ZonedDateTime.of(ldt, utcZone);
+        ZonedDateTime local = utc.withZoneSameInstant(localZone);
+
+        return Timestamp.valueOf(local.toLocalDateTime());
     }
 }
